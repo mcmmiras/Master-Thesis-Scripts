ca158dfd23b3fa2c9b514f4ff51ae353a3a30317
Mari <mmartinmiras@uoc.edu>
2024-11-13 14:22:09 +0100

    added function to calculate the stickiness scale of residues and graphically representate it

--- descriptors/calculate_environment_stickiness.py
+++ descriptors/calculate_environment_stickiness.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import os
 
 # Usage: pymol -qc script.py
 # Usage: python script.py fullPDB_path PDB_list(.txt)
@@ -14,6 +15,7 @@ from math import sqrt, log
 from pymol import *
 import sys
 import subprocess
+import matplotlib.pyplot as plt
 
 # ARGUMENTS:
 fullPDB = sys.argv[1]
@@ -24,7 +26,10 @@ if not "output_rASA_files" in os.listdir(dir):
     subprocess.run(f"mkdir output_rASA_files/", shell=True)
 if not "output_split_chains" in os.listdir(dir):
     subprocess.run(f"mkdir output_split_chains/", shell=True)
+if not "output_stickiness_scales" in os.listdir(dir):
+    subprocess.run(f"mkdir output_stickiness_scales/", shell=True)
 errors = []
+
 
 # FUNCTIONS:
 # Function 1: create dataframe for each PDB.
@@ -93,7 +98,40 @@ def assign_location():
         if df_pdb.loc[i, ">25_rASAc"] == "no" and df_pdb.loc[i, "delta"] > 0 and df_pdb.loc[i, ">25_rASAu"] == "yes":
             df_pdb.at[i, "location"] = "interface"
 
-# Function 5: calculate log of location propensity per residue.
+#Function 5: calculate log of location propensity per residue.
+def calculate_residues_stickiness(df_pdb):
+    # First, we will create the dataframe to store the stickiness scale:
+    df_res = pd.DataFrame(data={"residue": ["ALA", "CYS", "ASP", "GLU", "PHE", "GLY", "HIS", "ILE", "LYS",
+                                                         "LEU", "MET", "ASN", "PRO", "GLN", "ARG", "SER", "THR", "VAL",
+                                                         "TRP",
+                                                         "TYR"]})
+    df_res.set_index('residue', inplace=True)
+    df_res["freq_interface"] = 0
+    df_res["freq_surface"] = 0
+    df_res["log(freq_interface/freq_surface)"] = 0
+    # Then, we will identify the PDB file and read its corresponding output with rASA values:
+    pdb = df_pdb[:4]
+    df_pdb = pd.read_csv(os.path.join(os.getcwd(),f"output_rASA_files/{df_pdb}"), sep="\t", header=(0))
+    # Iteration to calculate frequencies and log between interface/surface:
+    for i, row in df_pdb.iterrows():
+        residue = df_pdb.iloc[i, 1]
+        location = df_pdb.iloc[i,9]
+        if location == "interface":
+            df_res.at[residue,"freq_interface"] = df_res.loc[residue,"freq_interface"]+1
+        elif location == "surface":
+            df_res.at[residue,"freq_surface"] = df_res.loc[residue,"freq_surface"]+1
+    for i,row in df_res.iterrows():
+        if df_res.loc[i,"freq_interface"] != 0 and df_res.loc[i,"freq_surface"] !=0:
+            log = (math.log(df_res.loc[i,"freq_interface"]/df_res.loc[i,"freq_surface"]))
+            df_res.at[i,"log(freq_interface/freq_surface)"] = log
+    # Graphical representation of the calculated stickiness scale:
+    plt.figure(figsize=(10, 10))
+    df_res.plot(y="log(freq_interface/freq_surface)", use_index=True, kind="bar")
+    plt.axhline(0, color='k')
+    plt.savefig(f"./output_stickiness_scales/{pdb}_stickiness_scale.png")
+    # Finally, we will save our results:
+    df_res.to_csv(f"./output_stickiness_scales/{pdb}_stickiness_scale.csv", sep = "\t")
+
 
 
 
@@ -101,41 +139,42 @@ def assign_location():
 pymol.finish_launching(["pymol","-qc"]) # Run PyMol from script without opening the GUI
 # Iterate all PDBs from a given list:
 for i in df_list.index:
+    pdb = df_list['pdb'][i]
     try:
-        pdb = df_list['pdb'][i]
-        df_pdb = create_dataframe()
-        ent_file = f"pdb{pdb}.ent" # General use for fullPDB
-        ent_file = os.path.join(fullPDB, ent_file)
-        residues_names = []
-        # Parsing of PDB file for storing the residue names:
-        try:
-            structure = parser.get_structure(pdb, ent_file)
-            residues_obj = Structure.Structure.get_residues(structure)
-            for res in residues_obj:
-                if res != "HOH":
-                    residues_names.append(Residue.Residue.get_resname(res))
-        except:
-            print(f"Error in parsing {pdb}.")
-        try:
-            # Extraction of all chains separatedly:
-            df_pdb = extract_chains_rASA(ent_file,df_pdb,pdb,residues_names)
-        except:
-            print(f"Error in {pdb}: could not extract chains.")
-            errors.append(f"{pdb}\n")
-        try:
-            # Calculate delta (differences) between rASAc and rASAu values:
-            calculate_delta()
-        except:
-            print(f"Error in {pdb}: could not calculate delta.")
-            errors.append(f"{pdb}\n")
-        try:
-            # Assign residues' location within the folded structure:
-            assign_location()
-        except:
-            print(f"Error in {pdb}: could not assign location.")
-            errors.append(f"{pdb}\n")
-        # Store results in separated csv files for each PDB:
-        df_pdb.to_csv(f"./output_rASA_files/output_{pdb}.csv", sep="\t")
+        if f"{pdb}_rASA.csv" not in os.listdir(os.path.join(os.getcwd(),"output_rASA_files")):
+            df_pdb = create_dataframe()
+            ent_file = f"pdb{pdb}.ent" # General use for fullPDB
+            ent_file = os.path.join(fullPDB, ent_file)
+            residues_names = []
+            # Parsing of PDB file for storing the residue names:
+            try:
+                structure = parser.get_structure(pdb, ent_file)
+                residues_obj = Structure.Structure.get_residues(structure)
+                for res in residues_obj:
+                    if res != "HOH":
+                        residues_names.append(Residue.Residue.get_resname(res))
+            except:
+                print(f"Error in parsing {pdb}.")
+            try:
+                # Extraction of all chains separatedly and calculation of rASA in complexed and unbound states:
+                df_pdb = extract_chains_rASA(ent_file,df_pdb,pdb,residues_names)
+            except:
+                print(f"Error in {pdb}: could not extract chains.")
+                errors.append(f"{pdb}\n")
+            try:
+                # Calculate delta (differences) between rASAc and rASAu values:
+                calculate_delta()
+            except:
+                print(f"Error in {pdb}: could not calculate delta.")
+                errors.append(f"{pdb}\n")
+            try:
+                # Assign residues' location within the folded structure:
+                assign_location()
+            except:
+                print(f"Error in {pdb}: could not assign location.")
+                errors.append(f"{pdb}\n")
+            # Store results in separated csv files for each PDB:
+            df_pdb.to_csv(f"./output_rASA_files/{pdb}_rASA.csv", sep="\t")
     except:
         print(f"Error in {pdb}: could not create dataframe.")
         errors.append(f"{pdb}\n")
@@ -144,9 +183,19 @@ for i in df_list.index:
 cmd.quit()
 
 # Return PDB that have generated errors:
-with open("./output_rASA_files/unprocessed_PDBs.txt","w") as file:
+with open("./unprocessed_PDBs.txt","w") as file:
     file.writelines(errors)
     for error in errors:
         error = error.split(f"\n")[0]
-        subprocess.run(f"rm -r ./output_rASA_files/output_{error}.csv", shell=True)
+        subprocess.run(f"rm -r ./output_rASA_files/{error}_rASA.csv", shell=True)
     file.close()
+
+# Iterate through rASA output for each PDB file:
+for df_pdb in os.listdir(os.path.join(os.getcwd(), f"output_rASA_files")):
+    try:
+        pdb = df_pdb[:4]
+        if not f"{pdb}_stickiness_scale.csv" in os.listdir(os.path.join(os.getcwd(), f"output_stickiness_scales")):
+            # Calculate the stickiness scale for each PDB file:
+            calculate_residues_stickiness(df_pdb)
+    except:
+        print(f"Error: the {df_pdb} file was not found.")
